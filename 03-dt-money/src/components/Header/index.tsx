c8c82f901d6fd3a78fad5313b6355ba678da2d57
Kleber Freire <ch4r4d4@gmail.com>
2023-02-06 16:06:13 -0300

    projeto 03 modulo 1 ok

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,14 +1,24 @@
 import { NewTransactionButton, HeaderContainer, HeaderContent } from "./styles";
 import logoImg from "../../assets/logo.svg";
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header () {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="logo do dt-money" />
-        <NewTransactionButton>
-          Nova transação
-        </NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>
+              Nova transação
+            </NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+     
       </HeaderContent>
     </HeaderContainer>
   )
